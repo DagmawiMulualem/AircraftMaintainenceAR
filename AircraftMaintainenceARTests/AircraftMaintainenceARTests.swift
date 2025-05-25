88b5340f0e88af11792f5f14e7ef20a69e24cf08
Dagmawi Mulualem <Dagmawi.m.mulualem@gmail.com>
2025-05-25 01:36:03 -0400

    Initial Commit

---
+++ AircraftMaintainenceARTests/AircraftMaintainenceARTests.swift
@@ -0,0 +1,17 @@
+//
+//  AircraftMaintainenceARTests.swift
+//  AircraftMaintainenceARTests
+//
+//  Created by Dagmawi Mulualem on 5/25/25.
+//
+
+import Testing
+@testable import AircraftMaintainenceAR
+
+struct AircraftMaintainenceARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
